99c2e685bb2c40c2b79c51f7681f52b66a6ae22b
Liliana <lilianasajeajdarova@gmail.com>
2026-02-18 00:05:22 +0300

    2 лаба

---
+++ server/routes/offerRoutes.js
@@ -0,0 +1,15 @@
+import {Router} from 'express';
+import upload from '../middleware/upload.js';
+import { createOffer, getAllOffers } from '../controllers/offerController.js';
+import { getFullOffer } from '../controllers/offerController.js';
+
+const router = new Router();
+
+router.get('/offers', getAllOffers);
+router.post('/offers', upload.fields([
+    {name:'previewImage', maxCount: 1 }, 
+    {name: 'photos', maxCount: 6 }
+]), createOffer);
+router.get('/offers/:id', getFullOffer);
+
+export default router;
